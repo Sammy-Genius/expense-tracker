6233fcd2b6adaadf5570df0c4c6032c15d840a63
William M. Tsikata <tsikatawill@gmail.com>
2022-11-11 23:28:41 +0000

    chore: add placeholder landing page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import { Button, Container } from "./components";
+
 function App() {
+  const navigate = useNavigate();
   return (
     <div className="App">
-      
+      <Container size="xl">
+        <h1>Landing page</h1>
+        <br />
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas fugiat
+          nesciunt itaque, corrupti deleniti asperiores a sapiente nobis quidem
+          quo quos delectus debitis aperiam ipsum, architecto quasi facilis.
+          Molestiae, accusantium!
+        </p>
+        <br />
+        <Button onClick={() => navigate("/login")}>Login</Button>
+      </Container>
     </div>
   );
 }
